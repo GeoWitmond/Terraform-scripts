75118c187cc404f6e12caad3a0e89e85783297eb
Geo <89748991+GeoWitmond@users.noreply.github.com>
2025-03-10 15:00:56 +0100

    Update promote_dc.ps1

--- promote_dc.ps1
+++ promote_dc.ps1
@@ -1,24 +1,19 @@
 param (
     [string]$DomainName,
     [string]$NetbiosName,
-    [string]$SafeModePassword,
-    [bool]$IsFirstDC
+    [string]$SafeModePassword
 )
+
+# Install AD Domain Services
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 
 # Convert password to a Secure String
 $SecureSafeModePassword = ConvertTo-SecureString $SafeModePassword -AsPlainText -Force
 
-if ($IsFirstDC) {
-    Write-Host "Configuring first Domain Controller in new forest: $DomainName"
-
-    # Install AD DS role
-    Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
-
-    # Promote to first Domain Controller
-    Install-ADDSForest `
-        -DomainName $DomainName `
-        -DomainNetbiosName $NetbiosName `
-        -SafeModeAdministratorPassword $SecureSafeModePassword `
-        -Force `
-        -NoRebootOnCompletion
-}
+# Promote server to Domain Controller
+Install-ADDSForest `
+    -DomainName $DomainName `
+    -DomainNetbiosName $NetbiosName `
+    -SafeModeAdministratorPassword $SecureSafeModePassword `
+    -Force `
+    -NoRebootOnCompletion

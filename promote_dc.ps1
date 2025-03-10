b2555db82a3d5142b2b0a6a155167a3b0595c2e7
Geo <89748991+GeoWitmond@users.noreply.github.com>
2025-03-10 15:26:00 +0100

    Update promote_dc.ps1

--- promote_dc.ps1
+++ promote_dc.ps1
@@ -1,38 +1,19 @@
 param (
     [string]$DomainName,
     [string]$NetbiosName,
-    [string]$SafeModePassword,
-    [bool]$IsFirstDC,
-    [string]$DomainAdminUser,
-    [string]$DomainAdminPassword
+    [string]$SafeModePassword
 )
-
-# Convert passwords to Secure Strings
-$SecureSafeModePassword = ConvertTo-SecureString $SafeModePassword -AsPlainText -Force
-$SecureDomainAdminPassword = ConvertTo-SecureString $DomainAdminPassword -AsPlainText -Force
-$DomainAdminCredentials = New-Object System.Management.Automation.PSCredential("$DomainName\$DomainAdminUser", $SecureDomainAdminPassword)
 
 # Install AD Domain Services
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 
-if ($IsFirstDC) {
-    Write-Host "Configuring first Domain Controller in new forest: $DomainName"
+# Convert password to a Secure String
+$SecureSafeModePassword = ConvertTo-SecureString $SafeModePassword -AsPlainText -Force
 
-    # Promote to first Domain Controller
-    Install-ADDSForest `
-        -DomainName $DomainName `
-        -DomainNetbiosName $NetbiosName `
-        -SafeModeAdministratorPassword $SecureSafeModePassword `
-        -Force `
-        -NoRebootOnCompletion
-} else {
-    Write-Host "Adding this server as a Replica Domain Controller to domain: $DomainName"
-
-    # Promote to Replica DC
-    Install-ADDSDomainController `
-        -DomainName $DomainName `
-        -Credential $DomainAdminCredentials `
-        -SafeModeAdministratorPassword $SecureSafeModePassword `
-        -Force `
-        -NoRebootOnCompletion
-}
+# Promote server to Domain Controller
+Install-ADDSForest 
+    -DomainName $DomainName 
+    -DomainNetbiosName $NetbiosName 
+    -SafeModeAdministratorPassword $SecureSafeModePassword 
+    -Force 
+    -NoRebootOnCompletion

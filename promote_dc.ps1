2b649a9f750dfced91351bb155fab5a593e89049
Geo <89748991+GeoWitmond@users.noreply.github.com>
2025-03-10 14:00:20 +0100

    Update promote_dc.ps1

--- promote_dc.ps1
+++ promote_dc.ps1
@@ -1,19 +1,38 @@
 param (
     [string]$DomainName,
     [string]$NetbiosName,
-    [string]$SafeModePassword
+    [string]$SafeModePassword,
+    [bool]$IsFirstDC
 )
-
-# Install AD Domain Services
-Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 
 # Convert password to a Secure String
 $SecureSafeModePassword = ConvertTo-SecureString $SafeModePassword -AsPlainText -Force
 
-# Promote server to Domain Controller
-Install-ADDSForest `
-    -DomainName $DomainName `
-    -DomainNetbiosName $NetbiosName `
-    -SafeModeAdministratorPassword $SecureSafeModePassword `
-    -Force `
-    -NoRebootOnCompletion
+if ($IsFirstDC) {
+    Write-Host "Configuring first Domain Controller in new forest: $DomainName"
+
+    # Install AD DS role
+    Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+
+    # Promote to first Domain Controller
+    Install-ADDSForest `
+        -DomainName $DomainName `
+        -DomainNetbiosName $NetbiosName `
+        -SafeModeAdministratorPassword $SecureSafeModePassword `
+        -Force `
+        -NoRebootOnCompletion
+}
+else {
+    Write-Host "Adding this server as a Replica Domain Controller to domain: $DomainName"
+
+    # Install AD DS role
+    Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+
+    # Promote to Replica DC
+    Install-ADDSDomainController `
+        -DomainName $DomainName `
+        -Credential (Get-Credential -UserName "$DomainName\Administrator" -Message "Enter domain admin credentials") `
+        -SafeModeAdministratorPassword $SecureSafeModePassword `
+        -Force `
+        -NoRebootOnCompletion
+}
